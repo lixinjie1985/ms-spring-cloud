87201d1c50d7131781c4afbccac2afeb160ff4c0
lixinjie1985 <lixinjie1985@126.com>
2018-09-07 15:01:45 +0800

    init

---
+++ src/main/java/com/sc/ms/SpringCloudMsApplication.java
@@ -0,0 +1,12 @@
+package com.sc.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudMsApplication.class, args);
+	}
+}
